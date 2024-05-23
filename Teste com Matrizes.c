3689a8b9a056ad5747d9e945b351ae7b9d5bbb84
Wiliam Cherubim <112710340+wiliamcherubim@users.noreply.github.com>
2024-05-23 10:44:48 -0300

    Matrix test
    Teste com matrizes

---
+++ Teste com Matrizes.c
@@ -0,0 +1,28 @@
+// Code file created by C Code Develop
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(){
+    int matriz[3][3], i, j;
+    
+    printf("\nDigite valor para os elementos da matriz \n\n" );
+    
+    for(i = 0; i < 3;  i++)
+    		for(j =0;  j < 3;  j++)    
+   	  {
+        		printf("\nElemento[%d][%d] = ", i, j);
+        		scanf("%d", &matriz[i][j]);
+    		}
+    
+    printf("\n\n*************** Saida de Dados *************** \n\n");
+    
+    for(i = 0; i < 3;  i++)
+    		for(j = 0;  j < 3;  j++)    
+    		{
+        		printf("\nElemento[%d][%d] = %d", i,j,matriz[i][j]);
+        
+        
+    		}
+    return 0;
+}
